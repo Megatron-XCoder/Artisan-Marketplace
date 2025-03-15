aba1aecb9797df0f319c5d2abb69001b2da1a9eb
Sanjeev Kumar Das <crisiscrush525@gmail.com>
2025-03-15 16:53:54 +0530

    changed path names

--- frontend/src/Components/Route/ProductDetailsCard/ProductDetailsCard.jsx
+++ frontend/src/Components/Route/ProductDetailsCard/ProductDetailsCard.jsx
@@ -7,7 +7,6 @@ import {
 } from "react-icons/ai";
 import {RxCross1} from "react-icons/rx";
 import {Link} from "react-router-dom";
-import styles from "../../../styles/styles";
 // import { useDispatch, useSelector } from "react-redux";
 import {toast} from "react-toastify";
 import {backend_url} from "../../../server.jsx";
